c1d4e92a943a7f2eaa4ce6a6921ff032ee9fdeb6
appy-hour <81193735+rebecafelicissimo@users.noreply.github.com>
2022-02-21 10:09:14 -0300

    testing mobile modal

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 
-function emAndamento() {
+/* function emAndamento() {
   alert("Logo o encurtador de links estará disponível para uso. Estamos desenvolvendo-o. Obrigada!");
+} */
+
+function showModal() {
+  const modalElement = document.getElementById("modal");
+  modalElement.classList.add("show-modal");
+}
+
+function hideModal() {
+  const modalElement = document.getElementById("modal");
+  modalElement.classList.remove("show-modal");
 }
